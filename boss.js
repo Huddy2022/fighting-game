63bea5f2ba8a260ffb423a98d145cbd771ac6677
Huddy2022 <mlvnhud@aol.com>
2023-11-11 14:28:39 +0000

    add pre game timer to all games

--- boss.js
+++ boss.js
@@ -169,8 +169,6 @@ const keys = {
 
 }
 
-decreaseTimer()
-
 let enemyAttackCooldown = 0;
 let attackCooldownDuration = 60; // Cooldown duration in frames (assuming 60 FPS)
 
@@ -257,6 +255,35 @@ function updateEnemyAI() {
     enemy.update();
 
 
+}
+
+let preGameTimer = 3;
+let preGameText = 'FIGHT!';
+let preGameTimerId;
+
+function startPreGameTimer() {
+    const preGameTextElement = document.querySelector('#preGameText');
+    preGameTimerId = setInterval(() => {
+        if (preGameTimer > 0) {
+            document.querySelector('#preGameTimer').innerHTML = preGameTimer;
+            preGameTimer--;
+        } else {
+            preGameTextElement.innerHTML = preGameText;
+            document.querySelector('#preGameTimer').style.display = 'none';
+            clearInterval(preGameTimerId);
+
+            // Optionally: Hide the preGameText after a short delay
+            setTimeout(() => {
+                preGameTextElement.style.display = 'none';
+                startGame();
+            }, 1000);
+        }
+    }, 1000);
+}
+
+function startGame() {
+    decreaseTimer();
+    animate();
 }
 
 function animate() {
@@ -359,8 +386,11 @@ function newGame() {
     player.health = 100; // Reset player health
     enemy.health = 100; // Reset enemy health
 
-    // Start the game loop
-    animate();
+    // Show the canvas and hide the pre-game timer
+    document.querySelector('#preGameTimer').style.display = 'block';
+
+    // Start the pre-game timer
+    startPreGameTimer();
 }
 
 newGame()
